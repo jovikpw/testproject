53c60adec0f4c802bd6782c6696b063479a5ac33
Abdullaeva Sevara <araves_99@mail.ru>
2019-05-05 18:52:32 +0500

    Update about.blade.php

--- resources/views/other/about.blade.php
+++ resources/views/other/about.blade.php
@@ -8,7 +8,8 @@
     </div>
     <div class="row">
         <div class="col-md-12">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda commodi illum nobis nostrum numquam offici</p>
+            <p>SEEEEEEEVAA
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda commodi illum nobis nostrum numquam offici</p>
         </div>
     </div>
 
